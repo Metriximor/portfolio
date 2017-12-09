884b59ada6058164c8d42f9f196353f5faef5341
Metriximor <pedroaocalhas@gmail.com>
2017-12-09 22:37:18 +0000

    acabei o exercicio
    https://repl.it/@Metriximor/Ficha4-6

--- ISEP/APROG/C/Ficha4/ex6.c
+++ ISEP/APROG/C/Ficha4/ex6.c
@@ -1,7 +1,6 @@
-#include<stdio.h>
-#include<string.h>
+//ISTO FAZ-SE MUITO MAIS FACILMENTE COM ESTRUTURAS
 
-//NAO ESTA ACABADO
+#include<stdio.h>
 
 int lerInteiro1(int linf)
 {
@@ -18,20 +17,20 @@ int lerInteiro1(int linf)
 
 int lerInteiro2(int linf, int lsup)
 {
-  int num;
+  int num=0;
   do
   {
     printf("Introduza um valor superior a %d e inferior a %d >:", linf, lsup);
     scanf("%d", &num);
   }
-  while(num<linf || num>lsup);
+  while(num<(linf+1) || num>(lsup-1));
+  //printf("NUM: %d", num);
   return num;
-  //Debug: printf("NUM: %d", num);
 }
 
 float lerReal(float linf)
 {
-  float num;
+  float num=0;
   do
   {
     printf("Introduza um valor superior ou igual a %.2f >:", linf);
@@ -39,7 +38,7 @@ float lerReal(float linf)
   }
   while(num<linf);
   return num;
-  Debug: printf("NUM: %f", num);
+  //Debug: printf("NUM: %f", num);
 }
 
 char lerFM()
@@ -47,26 +46,42 @@ char lerFM()
   char gen;
   do 
   {
+    //as proximas duas linhas servem para limpar o stdin
+    fflush(stdin);
+    __fpurge(stdin);
+    
+    //Aqui é q verifica
     printf("Introduza o genero >:");
     scanf("%c", &gen);
   }
-  while(gen!='F' || gen!='f' || gen!='M' || gen!='m');
+  //aqui tem que ser && para que as condições se verifiquem
+  while(gen!='F' && gen!='f' && gen!='M' && gen!='m');
   return gen;
 }
 
 void main()
 {
-  int n, cod[n], ano[n];
-  char gen[n];
-  float sal[n];
+  //Isto seria feito muito melhor com estruturas
+  int n;
   printf("Quantos funcionarios?");
   scanf("%d", &n);
-  for(int i= 0; i < n; i++)
+  int i, cod[n], ano[n];
+  char gen[n];
+  float sal[n];
+  for(i= 0; i < n; i++)
   {
     printf("------- Funcionario %d -------\n", i+1);
-    ano[i]=lerInteiro2(0, 50);
-    cod[i]=lerInteiro2(0, 9999);
+    cod[i]=lerInteiro2(0,9999);
+    ano[i]=lerInteiro2(0,50);
     sal[i]=lerReal(250.50);
     gen[i]=lerFM();
+  }
+  for(i=0;i<n;i++)
+  {
+    printf("------- Funcionario %d -------\n", i+1);
+    printf("O codigo do trabalhador e: %d\n",cod[i]);
+    printf("O trabalhador esta na empresa a: %d anos\n",ano[i]);
+    printf("O salario e: %.2f\n",sal[i]);
+    printf("O sexo do trabalhador e: %c\n",gen[i]);
   }
 }
