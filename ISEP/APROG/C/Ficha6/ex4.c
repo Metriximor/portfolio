6dcd53e49e2e2863caba63cd86506449ba608408
Metriximor <pedroaocalhas@gmail.com>
2017-12-27 19:19:15 +0000

    Update ex4.c

--- ISEP/APROG/C/Ficha6/ex4.c
+++ ISEP/APROG/C/Ficha6/ex4.c
@@ -1,176 +1,71 @@
+//Librarias
 #include<stdio.h>
 #include<string.h>
 
-//Defines vão aqui, nota ao criar um array com algum define nao esquecer de subtrair 1
-#define MAX_CHARACTERS 50 //50
-#define MAX_EQUIPAS 6 //6, nao se pode definir mais de 54 equipas
-#define MAX_ALUNOSEQUIPA 8 //8
-#define MAX_ATIVIDADESALUNO 5 //5
+//Dicionário
+#define NUMALUNOS 1 //Nota:Ao definir arrays com este valor, nao esquecer de subtrair 1
 
-//Structs vão aqui, todas as structs começam com o nome st_ para indicar que é o nome da struct sem typedef
-typedef struct st_atividade{
-    int idAtividade;
-    int contadorCorreto;
-    //Assumindo que este é o tempo que todos os alunos demoraram a completar a atividade
-    int tempo;
-}atividade;
-
-typedef struct st_aluno{
-    char nome[MAX_CHARACTERS+1];
-    //Têm que ser restrita a valores realistas mais tarde.
-    int idade;
-    char genero;
-    atividade ativ[MAX_ATIVIDADESALUNO-1][MAX_EQUIPAS];
-}aluno;
-
-typedef struct st_equipa{
-    char sigla[MAX_CHARACTERS+1];
-    char localidade[MAX_CHARACTERS+1];
-    aluno alu[MAX_ALUNOSEQUIPA-1];
-}equipa;
-
-//Funções vão aqui
-
-void escreverAluno(aluno vetAluno[], int *contadorAluno)
+//Estruturas
+typedef struct disciplina
 {
-    //Debug: printf("%d %d\n", *contadorAluno, MAX_ALUNOSEQUIPA*MAX_EQUIPAS);
-    if(*contadorAluno>=(MAX_ALUNOSEQUIPA*MAX_EQUIPAS))
-    {
-        printf("Numero maximo de alunos alcancado.");
-        voltarAoMenu();
-    }
-    else
-    {
-        printf("Qual o nome do aluno %d\n>:",*contadorAluno+1);
-        fgets(vetAluno[*contadorAluno].nome, MAX_CHARACTERS, stdin);
-        fflush(stdin);
-        printf("Qual a idade do aluno?\n");
-        //Aqui faz uma verificação de sanidade para que ninguem coloque idades absurdas nos estudantes
-        do{
-            printf(">:");
-            scanf("%d", &vetAluno[*contadorAluno].idade);
-            //O fflush está aqui para o caso de ser introduzido em acidente(ou nao) um caracter, permitindo assim a introduçao de um integer
-            fflush(stdin);
-        }while(vetAluno[*contadorAluno].idade<=16 || vetAluno[*contadorAluno].idade>=90);
+  int nota;
+  char nome[10];
+} tipo_disciplina;
 
-        (*contadorAluno)++;
+typedef struct aluno
+{
+  char nome[10];
+  char apelido[10];
+  int idade;
+  tipo_disciplina disc;//Diminutivo de disciplina
+} aluno_tipo;
 
-        //Esta função é dedicada a voltar ao menu principal de forma correta.
-        voltarAoMenu();
-    }
-}
-void mostrarAluno()
+//Funções
+void preencherStruct(aluno_tipo aluno[])
 {
-    voltarAoMenu();
-}
-void apagarAluno()
-{
-    voltarAoMenu();
+  int i=0;
+  for(i=0; i<NUMALUNOS; i++)
+  {
+    //Vou precisar de nome, apelido, idade, disc.nome, disc.nota
+    
+    printf("Introduza o nome do aluno %d\n>:", i+1);
+    fgets(aluno[i].nome, 10, stdin);
+    //Para Debug- printf("%s", aluno->nome);
+    __fpurge(stdin);
+    
+    printf("Introduza o apelido do aluno %d\n>:", i+1);
+    fgets(aluno[i].apelido, 10, stdin);
+    __fpurge(stdin);
+    
+    printf("Introduza a idade do aluno %d\n>:", i+1);
+    scanf("%d", &aluno[i].idade);
+    //Precisamos destas duas linhas para limpar o buffer do teclado
+    __fpurge(stdin);
+    
+    printf("Introduza o nome da disciplina %d\n>:", i+1);
+    fgets(aluno[i].disc.nome, 10, stdin);
+    __fpurge(stdin);
+    
+    printf("Introduza a nota na disciplina %d\n>:", i+1);
+    scanf("%d", &aluno[i].disc.nota);
+    __fpurge(stdin);
+  }
 }
 
-void escreverAtividade()
+void mostrarAluno(aluno_tipo aluno[])
 {
-    voltarAoMenu();
-}
-void mostrarAtividade()
-{
-    voltarAoMenu();
-}
-void apagarAtividade()
-{
-    voltarAoMenu();
+  int i;
+  for(i=0; i<NUMALUNOS; i++)
+  {
+    printf("-------------------\nAluno %d\nNome: %sApelido: %sIdade: %d\nNome Disciplina: %sNota na disciplina: %d\n", i+1, aluno[i].nome, aluno[i].apelido, aluno[i].idade, aluno[i].disc.nome, aluno[i].disc.nota);
+  }
+  printf("-------------------");
 }
 
-void mostrarAlunosEquipa(/*sigla equipa*/)
-{
-    voltarAoMenu();
-}
-
-void totalRespostasCerta()
-{
-    voltarAoMenu();
-}
-void mediaRespostasCerta()//media por cada atividade
-{
-    voltarAoMenu();
-}
-
-void mediaIdadesEquipa()
-{
-    voltarAoMenu();
-}
-
-void menosTempo()
-{
-    voltarAoMenu();
-}
-
-void listarAlfabeticamente()
-{
-    voltarAoMenu();
-}
-
-/**Funções adicionais não exigidas no enunciado**/
-void criarEquipa()
-{
-    voltarAoMenu();
-}
-
-void voltarAoMenu()
-{
-    printf("\nPrima ENTER para voltar ao menu principal...\n");
-    getchar();
-}
-
-void debug(int *contadorAlunos)
-{
-    printf("Pode ser registado um numero maximo de %d equipas", MAX_EQUIPAS);
-    printf("\nPode ser registados um numero maximo de %d alunos", (MAX_ALUNOSEQUIPA*MAX_EQUIPAS));
-    printf("\nHa %d aluno(s) registado(s).", *contadorAlunos);
-    voltarAoMenu();
-}
-
+//Função Inicial
 void main()
 {
-    short int menu;
-    int contadorAlunos=0;
-    //Aqui criamos uma matriz de 1's e 0's que vai guardar a informação de que equipa e que atividade é que um dado aluno está registado, o tamanho maximo de X é o numero maximo de atividades, que é o numero maximo de alunos vezes o numero maximo de atividades por aluno
-    //int matrizAtividadesEquipaAluno[][]}
-    //Declaração do vetor das 6 equipas e 48 alunos(numero max de equipas*numero maximo de alunos por equipa)
-    equipa vetEquipas[MAX_EQUIPAS-1];
-    aluno vetAlunos[(MAX_ALUNOSEQUIPA*MAX_EQUIPAS)-1];
-    do{
-        //Este fflush serve para limpar no caso de enquanto havia a mensagem "Enter para voltar ao menu" ter sido introduzido algo para alem do enter.
-        fflush(stdin);
-        //Este printf está dividido em 2: o primeiro printf são as funções requesitadas no enunciado e o segundo são funcões ou comandos extra para o caso de alguma necessidade nao especificada no enunciado.
-        printf("------Gestao Equipas------\nEscolha a opcao introduzindo o valor indicado\n1- Escrever um novo aluno\n2- Mostrar um aluno\n3- Apagar um aluno\n4- Escrever uma nova atividade\n5- Mostrar uma atividade\n6- Apagar uma atividade\n7- Mostrar os alunos de uma determinada equipa\n8- Total de respostas certas de uma equipa\n9- Media de respostas certas de uma equipa\n10- Media de idades de uma equipa\n11- Mostrar a equipa com menos tempo gasto numa determinada atividade\n12- Listar as equipas alfabeticamente\n");
-        printf("13- Criar uma nova equipa[So funciona %d vezes no inicio do programa]\n14- Debug\n15- Sair do programa\n--------------------------\n", MAX_EQUIPAS);
-        do{
-            printf(">:");
-            scanf("%d", &menu);
-            //O fflush está aqui para o caso de ser introduzido em acidente(ou nao) um caracter, permitindo assim a introduçao de um integer
-            fflush(stdin);
-        }while(menu<=0 || menu>=16);
-        switch(menu)
-        {
-            //Nota, os break são necessários senão o programa corre todos os comandos que há, depois de cada comando correr há necessidade de dar reset de menu para = 0 senão se fosse introduzido algum caracter o programa assumiria o valor que tinha previamente e correria essa opção de novo
-            case 1: escreverAluno(vetAlunos, &contadorAlunos); menu=0; break;
-            case 2: mostrarAluno(); menu=0; break;
-            case 3: apagarAluno(); menu=0; break;
-            case 4: escreverAtividade(); menu=0; break;
-            case 5: mostrarAtividade(); menu=0; break;
-            case 6: apagarAtividade(); menu=0; break;
-            case 7: mostrarAlunosEquipa(); menu=0; break;
-            case 8: totalRespostasCerta(); menu=0; break;
-            case 9: mediaRespostasCerta(); menu=0; break;
-            case 10: mediaIdadesEquipa(); menu=0; break;
-            case 11: menosTempo(); menu=0; break;
-            case 12: listarAlfabeticamente(); menu=0; break;
-            case 13: criarEquipa(); menu=0; break;
-            case 14: debug(&contadorAlunos); menu=0; break;
-            case 15: menu=15; break;
-            //O default está aqui se por alguma razão alguma coisa de mal acontecer ao int i o programa voltar ao menu, mas em teoria isto nunca deve correr.
-            default: menu=0;
-        }
-    }while(menu!=15);
+  aluno_tipo alunos[NUMALUNOS-1];
+  preencherStruct(alunos);
+  mostrarAluno(alunos);
 }
