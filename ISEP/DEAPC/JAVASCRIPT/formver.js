66e28bd29eebdea9fd94e842b66e2b556cd5e2a6
Metriximor <pedroaocalhas@gmail.com>
2018-06-11 20:31:31 +0100

    acabado

--- ISEP/DEAPC/JAVASCRIPT/formver.js
+++ ISEP/DEAPC/JAVASCRIPT/formver.js
@@ -1,10 +1,11 @@
 function verificacaoformulario() {
     var form = document.getElementById('formulario');
-    for (let i = 0; i < form.elements.length - 1; i += 2) {
+    var erro = false;
+    var contador = 0;
+    for (let i = 0; i < 10; i += 2) {
         var nome = form.elements[i].value;
         var idade = form.elements[i + 1].value;
-        var erro = false;
-        var contador = 1;
+        erro = false;
         var mensagemErro = "Erro: \n";
 
         if (!/^[a-zA-Z]+$/.test(nome)) {
@@ -15,10 +16,11 @@ function verificacaoformulario() {
             erro = true;
             mensagemErro = mensagemErro.concat("-Data inválida\n");
         }
-        if(nome=="" && idade=="") {
-            erro = falso;
+        if (nome == "" && idade == "" && i != 0) {
+            erro = false;
+            mensagemErro = "Erro: \n";
         }
-
+        contador++;
         //Debug: console.log(nome + ' ' + idade);
 
         if (erro) {
@@ -26,5 +28,16 @@ function verificacaoformulario() {
             return false;
             break;
         }
+    }
+    erro = true;
+    for (let i = 20; i < 22; i++) {
+        if (form.elements[i].checked) {
+            erro = false;
+        }
+    }
+    if (erro) {
+        mensagemErro = mensagemErro.concat("-Escolher método de ordenação")
+        alert(mensagemErro);
+        return false;
     }
 }
--- ISEP/DEAPC/JAVASCRIPT/novosocio.php
+++ ISEP/DEAPC/JAVASCRIPT/novosocio.php
@@ -8,21 +8,27 @@
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 
-<body>
+<body class="ultimoex">
     <?php 
-    $nome = $_POST['nome'];
-    $idade = $_POST['aniversario'];
-    $contador = 0;
+    $nome = array_filter($_POST['nome']);
+    $idade = array_filter($_POST['aniversario']);
+    $ordena = $_POST['ordena'];
+    
+    if($ordena=="nome") {
+        array_multisort($nome, SORT_NATURAL | SORT_FLAG_CASE, $idade);
+    }
+    else if($ordena=="data") {
+        array_multisort($idade, $nome, SORT_NATURAL | SORT_FLAG_CASE);
+    }
     
     $ficheiro = fopen("socios.dat", "w") or die("Erro ao abrir ficheiro!");
     
-    while($nome[$contador]!=null)
-    {
-        $contador++;
+    for($i = 0; $i < count($nome); $i++) {
+        $texto = $i+1 . ':' . $nome[$i] . ':' . $idade[$i] . "\n";
+        fwrite($ficheiro, $texto);
+        
+        echo $i+1 . ':' . $nome[$i] . ':' . $idade[$i] . "<br>";
     }
-    
-    //$texto = '1' . ':' . $nome . ':' . $idade . "\n";
-    //fwrite($ficheiro, $texto);
     fclose($ficheiro);    
     //Debug: echo($nome . ' ' . $idade); 
     ?>
