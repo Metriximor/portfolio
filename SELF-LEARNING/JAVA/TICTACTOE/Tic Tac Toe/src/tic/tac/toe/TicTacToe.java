06f02f3af38011533da8e67e8ba0bd921765f763
Metriximor <pedroaocalhas@gmail.com>
2018-10-03 20:48:56 +0100

    desenvolvi o gui
    e criei o começo de um verificador

--- SELF-LEARNING/JAVA/TICTACTOE/Tic Tac Toe/src/tic/tac/toe/TicTacToe.java
+++
@@ -1,9 +0,0 @@
-package tic.tac.toe;
-
-public class TicTacToe {
-
-    public static void main(String[] args) {
-        
-    }
-    
-}
---
+++ SELF-LEARNING/JAVA/TICTACTOE/Tic Tac Toe/src/tictactoe/Verificador.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+public class Verificador {
+    
+    private int matriz[][];
+    
+    int linhasColunas(int linha, int coluna) {
+        int pontos;
+        
+        //Nao esquecer de apagar isto
+        return 0;
+    }
+    
+}
