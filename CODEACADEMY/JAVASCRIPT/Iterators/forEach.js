c9859faa0c19be3394aa1140fb094f4ccbe73eb2
Pedro Ivo Carneiro Macieira <1170554@isep.ipp.pt>
2020-10-14 11:36:58 +0100

    portfolio restructuration

--- CODEACADEMY/JAVASCRIPT/Iterators/forEach.js
+++
@@ -1,7 +0,0 @@
-let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
-
-// Iterate over fruits below
-/*fruits.forEach(function(fruits) {
-  console.log('I want to eat a ' + fruits);
-});*/
-fruits.forEach(fruits => console.log('I want to eat a ' + fruits));
--- CODEACADEMY/JAVASCRIPT/Loops/while.js
+++
@@ -1,9 +0,0 @@
-let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
-let currentCard = 'Heart';
-
-while(currentCard!='Spade') {
-  console.log(currentCard);
-  currentCard = cards[Math.floor(Math.random() * 4)];
-}
-
-console.log('Found a spade!')
