4fc4c485dfd32e465bbd0f983c680a228a63be28
Metriximor <pedroaocalhas@gmail.com>
2021-04-27 05:11:16 +0100

    Update graficototalinfetadospormilhao.r

--- ISEP/ANADI/graficototalinfetadospormilhao.r
+++ ISEP/ANADI/graficototalinfetadospormilhao.r
@@ -2,19 +2,13 @@ library(readxl)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-# Lê os ficheiros do excel
-owid_covid_data <- read_excel("C:/Users/pedro/Desktop/ANADI/owid-covid-data.xlsx")
-view(owid_covid_data)
-# Agrupa por continente
-por_continente <- owid_covid_data %>% 
-  drop_na(continent, date, total_cases_per_million) %>% 
-  group_by(date, continent) %>% 
-  summarize(total_cases_per_million = sum(total_cases_per_million)) %>% 
-por_continente$date <- as.Date(por_continente$date, format="%Y-%m-%d")
-# Desenha o gráfico
-por_continente %>% 
-  ggplot(aes(x=date, y=total_cases_per_million, group=continent, colour=continent)) +
-  scale_y_continuous(trans = "log2") +
-  geom_line() +
-  ggtitle("Total de infetados por milhão de habitantes\nao longo do período de tempo, por continente.") +
-  labs(x="Data", y="Total de infetados por milhão", colour="Continente")
+read_excel("C:/Users/pedro/Desktop/ANADI/owid-covid-data.xlsx") %>% #Lê o ficheiro excel
+  drop_na(continent, date, total_cases_per_million) %>% # Apaga os campos vazios
+  group_by(date, continent) %>% # Agrupa por data e continente
+  summarize(total_cases_per_million = sum(total_cases_per_million)) %>% # Soma todos os casos por data e continente
+  mutate(date = as.Date(date, format="%Y-%m-%d")) %>% # Converte a data num formato correto
+  ggplot(aes(x=date, y=total_cases_per_million, group=continent, colour=continent)) + #Faz o plot do gráfico
+    scale_y_continuous(trans = "log2") + # Define a escala logaritmica
+    geom_line() + 
+    ggtitle("Total de infetados por milhão de habitantes\nao longo do período de tempo, por continente.") +
+    labs(x="Data", y="Total de infetados por milhão", colour="Continente")
